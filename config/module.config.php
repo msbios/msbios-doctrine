169e8d5ef40133da797a101c183b4f912580e4d1
Judzhin Miles <info@msbios.com>
2017-09-19 15:33:44 +0300

    Lazy Composer Commiter

--- config/module.config.php
+++ config/module.config.php
@@ -11,6 +11,19 @@ return [
         'configuration' => [
             'orm_default' => [
                 'types' => [
+                    // Some Custom Types
+                ],
+                'datetime_functions' => [
+                    'date' => ORM\Query\AST\Functions\DateFunction::class,
+                    'day' => ORM\Query\AST\Functions\DayFunction::class,
+                    'year' => ORM\Query\AST\Functions\YearFunction::class,
+                ],
+                'string_functions' => [
+                    'concat_ws' => ORM\Query\AST\Functions\ConcatWSFunction::class,
+                    'month' => ORM\Query\AST\Functions\MonthFunction::class,
+                ],
+                'numeric_functions' => [
+                    'geo' => ORM\Query\AST\Functions\GeoFunction::class
                 ],
             ],
         ],
@@ -18,6 +31,14 @@ return [
             'orm_default' => [
                 'driverClass' => \Doctrine\DBAL\Driver\PDOMySql\Driver::class,
                 'params' => [
+                    'host' => 'localhost',
+                    'user' => '',
+                    'password' => '',
+                    'dbname' => '',
+                    'charset' => 'utf8',
+                    'driverOptions' => [
+                        \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+                    ]
                 ]
             ],
         ],
