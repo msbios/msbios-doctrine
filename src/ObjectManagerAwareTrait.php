221a2d50d870b3e8da001bbfebd6ccb0745a16ec
Judzhin Miles <info@msbios.com>
2019-04-23 11:43:46 +0300

    Lazy Composer MSBios Commit Script

--- src/ObjectManagerAwareTrait.php
+++ src/ObjectManagerAwareTrait.php
@@ -1,39 +1,39 @@
 <?php
-/**
- * @access protected
- * @author Judzhin Miles <info[woof-woof]msbios.com>
- */
-namespace MSBios\Doctrine;
-
-use Doctrine\Common\Persistence\ObjectManager;
-
-/**
- * Trait ObjectManagerAwareTrait
- * @package MSBios\Doctrine
- * @deprecated DoctrineModule\Persistence\ProvidesObjectManager
- */
-trait ObjectManagerAwareTrait
-{
-    /** @var ObjectManager */
-    protected $objectManager;
-
-    /**
-     * @param ObjectManager $objectManager
-     * @return $this
-     */
-    public function setObjectManager(ObjectManager $objectManager)
-    {
-        $this->objectManager = $objectManager;
-        return $this;
-    }
-
-    /**
-     * Get the object manager
-     *
-     * @return ObjectManager
-     */
-    public function getObjectManager()
-    {
-        return $this->objectManager;
-    }
-}
+///**
+// * @access protected
+// * @author Judzhin Miles <info[woof-woof]msbios.com>
+// */
+//namespace MSBios\Doctrine;
+//
+//use Doctrine\Common\Persistence\ObjectManager;
+//
+///**
+// * Trait ObjectManagerAwareTrait
+// * @package MSBios\Doctrine
+// * @deprecated DoctrineModule\Persistence\ProvidesObjectManager
+// */
+//trait ObjectManagerAwareTrait
+//{
+//    /** @var ObjectManager */
+//    protected $objectManager;
+//
+//    /**
+//     * @param ObjectManager $objectManager
+//     * @return $this
+//     */
+//    public function setObjectManager(ObjectManager $objectManager)
+//    {
+//        $this->objectManager = $objectManager;
+//        return $this;
+//    }
+//
+//    /**
+//     * Get the object manager
+//     *
+//     * @return ObjectManager
+//     */
+//    public function getObjectManager()
+//    {
+//        return $this->objectManager;
+//    }
+//}
